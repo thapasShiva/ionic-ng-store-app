1edca184e32f657e662aba85b4fa0b8575d39757
Thapas <thapasvi.shiva@gmail.com>
2021-01-18 13:54:12 +0530

    Added get started
    Added get started

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,23 +2,29 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
+
   {
     path: 'home-page',
-    loadChildren: () => import('./pages/home-page/home-page.module').then( m => m.HomePagePageModule)
+    loadChildren: () => import('./pages/home-page/home-page.module').then(m => m.HomePagePageModule)
   },
   {
     path: 'get-started',
-    loadChildren: () => import('./pages/get-started/get-started.module').then( m => m.GetStartedPageModule)
-  }
+    loadChildren: () => import('./pages/get-started/get-started.module').then(m => m.GetStartedPageModule)
+  },
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  // },
+  {
+    path: '',
+    loadChildren: () => import('./pages/get-started/get-started.module').then(m => m.GetStartedPageModule)
+  },
 ];
+
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
